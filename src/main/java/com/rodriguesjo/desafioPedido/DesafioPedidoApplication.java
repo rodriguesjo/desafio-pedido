092c1c778c743902ed3b22e7f5e2674e850e861b
Jo Boettger <rodriguesjo8@outlook.com>
2025-01-18 21:47:55 -0300

    Primeiro commit

---
+++ src/main/java/com/rodriguesjo/desafioPedido/DesafioPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.rodriguesjo.desafioPedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPedidoApplication.class, args);
+	}
+
+}
